9396887626ceb92110493b6cd315d2b476702efc
rnjai <rnjailamba12@gmail.com>
2016-05-24 07:37:05 +0530

    Basic set up done now need to do demo of upload to AWS

--- app/src/users/Users.js
+++
@@ -1,7 +0,0 @@
-(function(){
-  'use strict';
-
-  // Prepare the 'users' module for subsequent registration of controllers and delegates
-  angular.module('starterApp', [ 'ngMaterial','ngAnimate','angular.filter','schemaForm','ngSchemaFormFile','ui.bootstrap']);
-
-})();
